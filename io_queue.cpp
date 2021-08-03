2643cc3cb5fb5fe38a9cb7464e8bf6a074c73172
Ed K <edk@illinois.edu>
2021-08-03 11:23:21 -0500

    uncomment size-fair code so both versions get compiled to help keep synchronized, update to match new Dequeue method signature, remove object files accidentally checked in, set .gitignore to avoid that

--- io_queue.cpp
+++ io_queue.cpp
@@ -350,7 +350,8 @@ int Random_Pick_a_TargetJob(uint64_t s[2], int *p_Idx_Job)
 	return (-1);
 }
 
-void* Func_thread_IO_Worker(void *pParam)	// process all IO wrok
+
+void* Func_thread_IO_Worker_LeiSizeFair(void *pParam)	// process all IO wrok
 {
 //	int i, idxTask, thread_id, idx_op, IdxMin, IdxMax, idx_JobRec, nNumQueuePerWorker, *pNext_IO_OP_Idx_Queue_List=NULL, range, nValid_Next_IO_OP=0;
 	int i, idxTask, thread_id, idx_op, IdxMin, IdxMax, idx_JobRec, nNumQueuePerWorker, range, nValid_Next_IO_OP=0;
@@ -572,8 +573,186 @@ void* Func_thread_IO_Worker(void *pParam)	// process all IO wrok
 }
 
 
-/*
-void* Func_thread_IO_Worker(void *pParam)	// process all IO wrok
+// IO worker thread implementing fair queue
+
+void* Func_thread_IO_Worker_FairQueue(void *pParam)
+{
+	//int i, idxTask, thread_id, idx_op, IdxMin, IdxMax, idx_JobRec, nNumQueuePerWorker, *pNext_IO_OP_Idx_Queue_List=NULL, range, nValid_Next_IO_OP=0;
+	int i, idxTask, thread_id, idx_op, IdxMin, IdxMax, idx_JobRec, nNumQueuePerWorker, range, nValid_Next_IO_OP=0;
+	IO_CMD_MSG Op_Msg, *pOP_Msg_Retrieve;
+	CIO_QUEUE *pIO_Queue=NULL;
+	//FIRSTOPLIST *pFirstOPList=NULL;
+	FIRSTOPLIST pFirstOPList[400];// need to make sure it is larger than range!!!!!
+	unsigned long int T_queue_Earlyest, T_queue_Earlyest_TargetJob;
+	int idx_Earlyest, idx_Earlyest_TargetJob, idx_rec_ht_Picked, nValidOPs, ToProcOP, IdxQueue_PreviousSelected=-1, idx_Cur;
+	struct timeval tm1, tm2;// tm1.tv_sec
+	long int t_accum=0;
+	long int nOp_Done=0;
+	struct timeval tm;
+
+	gettimeofday(&tm, NULL);
+	rseed[0] = tm.tv_sec;
+	rseed[1] = tm.tv_usec;
+
+	thread_id = *((int*)pParam);
+	printf("DBG> Func_thread_IO_Worker(): thread_id = %d\n", thread_id);
+	CoreBinding.Bind_This_Thread();
+
+	if(thread_id == 0){// the first thread is dedicated for inter-server communication via queue[0]
+		IdxMin = 0;
+		IdxMax = 0;
+		//sleep(36000);
+		pIO_Queue = &(IO_Queue_List[0]);
+		while(1){
+			if( (pIO_Queue->back) >= (pIO_Queue->front) ){// A queue that is not empty.
+				pIO_Queue->Dequeue(pOP_Msg_Retrieve);
+				memcpy(&Op_Msg, pOP_Msg_Retrieve, sizeof(IO_CMD_MSG));
+				fetch_and_add((int*)&(ActiveJobList[pOP_Msg_Retrieve->idx_JobRec].nOps_Done), 1);
+				Op_Msg.tid = thread_id;
+				
+				if (pthread_mutex_lock(&(Server_qp.pQP_Data[Op_Msg.idx_qp].qp_lock)) != 0) {
+					perror("pthread_mutex_lock");
+					exit(2);
+				}
+				
+				Process_One_IO_OP(&Op_Msg);// Do the real IO work!
+				nOPs_Done[thread_id]++;
+				
+				if (pthread_mutex_unlock(&(Server_qp.pQP_Data[Op_Msg.idx_qp].qp_lock)) != 0) {
+					perror("pthread_mutex_unlock");
+					exit(2);
+				}
+			}
+		}
+	}
+	else{
+		if( ( ( MAX_NUM_QUEUE - 1 ) % ( NUM_THREAD_IO_WORKER - 1 ) ) == 0 ){
+			nNumQueuePerWorker = ( MAX_NUM_QUEUE - 1 ) / ( NUM_THREAD_IO_WORKER - 1 ) ;
+		}
+		else{
+			nNumQueuePerWorker = ( MAX_NUM_QUEUE - 1 ) / ( NUM_THREAD_IO_WORKER - 1 ) + 1;
+		}
+		IdxMin = 1 + (thread_id-1)*nNumQueuePerWorker;
+		IdxMax = min( (IdxMin + nNumQueuePerWorker - 1), (MAX_NUM_QUEUE - 1));
+		printf("DBG> worker %d, (%d, %d)\n", thread_id, IdxMin, IdxMax);
+		range = IdxMax-IdxMin+1;
+
+		//pNext_IO_OP_Idx_Queue_List = (int*)malloc(sizeof(int)*range + sizeof(FIRSTOPLIST)*range);// the list of index to sorted list according to T_Queued
+		//assert(pNext_IO_OP_Idx_Queue_List != NULL);
+		//pFirstOPList = (FIRSTOPLIST *)((char*)pNext_IO_OP_Idx_Queue_List + sizeof(int)*range);
+
+		for(i=0; i<range; i++){
+			//pNext_IO_OP_Idx_Queue_List[i] = i;
+
+			pFirstOPList[i].idx_queue = -1;
+			pFirstOPList[i].idx_op = -1;
+			pFirstOPList[i].T_Queued = LARGE_T_QUEUED;
+		}
+
+	}
+	
+	IdxQueue_PreviousSelected = IdxMin - 1;
+	while(1){// loop forever
+		if(nActiveJob == 0)continue;
+		//gettimeofday(&tm1, NULL);
+		idx_rec_ht_Picked = Random_Pick_a_TargetJob(rseed, 0);
+		if(idx_rec_ht_Picked < 0)continue;
+
+		// loop over all queues this IO worker needs to cover and extract job info for the first OP
+		T_queue_Earlyest = T_queue_Earlyest_TargetJob = LARGE_T_QUEUED;
+		idx_Earlyest = idx_Earlyest_TargetJob = -1;
+		nValidOPs = 0;
+
+		for(i=1; i<=range; i++){// All IO worker handle queues independently now!!! No lock is needed now.
+			idx_Cur = IdxQueue_PreviousSelected + i;
+			if(idx_Cur > IdxMax)idx_Cur -= range;
+			idxTask = idx_Cur -IdxMin;
+
+			if(pFirstOPList[idxTask].idx_queue < 0){// An invalid record. Need to grab the first OP info. 
+				pIO_Queue = &(IO_Queue_List[idx_Cur]);
+				if( (pIO_Queue->back) >= (pIO_Queue->front) ){// A queue that is not empty.
+					pFirstOPList[idxTask].idx_queue = idx_Cur;
+					idx_op = pIO_Queue->front & IO_QUEUE_SIZE_M1;
+					pFirstOPList[idxTask].idx_op = idx_op;
+					pFirstOPList[idxTask].idx_rec_ht = pIO_Queue->pQueue_Data[idx_op].idx_JobRec;
+					pFirstOPList[idxTask].T_Queued = pIO_Queue->pQueue_Data[idx_op].T_Queued;
+
+					nValidOPs++;
+					// Need to insert this new record into the sorted list!!!!!!!!!!!!!!!!!!!!!!!
+				}
+				else{
+					pFirstOPList[idxTask].T_Queued = LARGE_T_QUEUED;
+				}
+			}
+
+			if(pFirstOPList[idxTask].T_Queued < LARGE_T_QUEUED){// a valid record!
+				if(pFirstOPList[idxTask].idx_rec_ht == idx_rec_ht_Picked){
+					if(pFirstOPList[idxTask].T_Queued < T_queue_Earlyest_TargetJob){
+						T_queue_Earlyest_TargetJob = pFirstOPList[idxTask].T_Queued;
+						idx_Earlyest_TargetJob = idxTask;
+					}
+				}
+				if(pFirstOPList[idxTask].T_Queued < T_queue_Earlyest){// find the earliest OP
+					T_queue_Earlyest = pFirstOPList[idxTask].T_Queued;
+					idx_Earlyest = idxTask;
+				}
+			}
+		}
+
+		//printf("DBG> Rank = %d idx_rec_ht_Picked = %d\n", mpi_rank, idx_rec_ht_Picked);
+		ToProcOP = 0;
+		if( idx_Earlyest_TargetJob >=0 ){// process the earliest target job request
+			IdxQueue_PreviousSelected = pFirstOPList[idx_Earlyest_TargetJob].idx_queue;
+			pIO_Queue = &(IO_Queue_List[IdxQueue_PreviousSelected]);
+			idx_op = pIO_Queue->front & IO_QUEUE_SIZE_M1;
+			ToProcOP = 1;
+			//printf("INFO> Proc target   OP %3d queue %4d Time %ld\n", pFirstOPList[idx_Earlyest_TargetJob].idx_queue, pIO_Queue->front, pFirstOPList[idx_Earlyest_TargetJob].T_Queued);
+			pFirstOPList[idx_Earlyest_TargetJob].idx_queue = -1;
+		}
+
+		else if( idx_Earlyest >=0 ){// process the earliest request then
+			IdxQueue_PreviousSelected = pFirstOPList[idx_Earlyest].idx_queue;
+			pIO_Queue = &(IO_Queue_List[IdxQueue_PreviousSelected]);
+			idx_op = pIO_Queue->front & IO_QUEUE_SIZE_M1;
+			ToProcOP = 1;
+			//printf("INFO> Proc earliest OP %3d queue %4d Time %ld\n", pFirstOPList[idx_Earlyest].idx_queue, pIO_Queue->front, pFirstOPList[idx_Earlyest].T_Queued);
+			pFirstOPList[idx_Earlyest].idx_queue = -1;
+		}
+
+
+		if(ToProcOP){
+			//gettimeofday(&tm2, NULL);
+			//t_accum += ( (tm2.tv_sec - tm1.tv_sec) * 1000000 + (tm2.tv_usec - tm1.tv_usec) );
+			nOp_Done++;
+			//if(nOp_Done % 100000 == 0){
+			//printf("INFO> thread_id = %d Overhead %5.3lf\n", thread_id, 1.0 * t_accum / nOp_Done);
+			//}
+
+			pIO_Queue->Dequeue(pOP_Msg_Retrieve);
+			memcpy(&Op_Msg, pOP_Msg_Retrieve, sizeof(IO_CMD_MSG));
+			fetch_and_add((int*)&(ActiveJobList[pOP_Msg_Retrieve->idx_JobRec].nOps_Done), 1);
+			Op_Msg.tid = thread_id;
+			
+			if (pthread_mutex_lock(&(Server_qp.pQP_Data[Op_Msg.idx_qp].qp_lock)) != 0) {
+				perror("pthread_mutex_lock");
+				exit(2);
+			}
+			
+			Process_One_IO_OP(&Op_Msg);// Do the real IO work!
+			nOPs_Done[thread_id]++;
+			
+			if (pthread_mutex_unlock(&(Server_qp.pQP_Data[Op_Msg.idx_qp].qp_lock)) != 0) {
+				perror("pthread_mutex_unlock");
+				exit(2);
+			}
+		}
+	}
+}
+
+
+// IO worker thread implementing FIFO queue
+
+void* Func_thread_IO_Worker_FIFO(void *pParam)	// process all IO wrok
 {
 	int i, thread_id, idx_op, IdxMin, IdxMax, idx_JobRec, nNumQueuePerWorker;
 	IO_CMD_MSG Op_Msg;
@@ -661,7 +840,20 @@ void* Func_thread_IO_Worker(void *pParam)	// process all IO wrok
 		}
 	}
 }
-*/
+
+
+// IO worker thread, either fair or FIFO
+void* Func_thread_IO_Worker(void *pParam)
+{
+
+	// return Func_thread_IO_Worker_LeiSizeFair(pParam);
+	// return Func_thread_IO_Worker_FairQueue(pParam);
+	return Func_thread_IO_Worker_FIFO(pParam);
+
+}
+
+
+
 void Process_One_IO_OP(IO_CMD_MSG *pOP_Msg)
 {
 	int Op_Tag;
