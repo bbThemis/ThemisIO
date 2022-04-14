0693cab576cd340e9b418afe2bbc8bd2088e5a61
Ishank Arora <ishankarora1100@utexas.edu>
2022-04-14 05:16:38 -0500

    updating rate to reflect rate on paper

--- testfair/test_tbf.sh
+++ testfair/test_tbf.sh
@@ -4,11 +4,11 @@ ppn=56
 iosize=1m
 
 IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 1)) \
-   ../tests/themis_client.sh -r 200 -u 101 -j 1001 -n 1 \
+   ../tests/themis_client.sh -r 20000 -u 101 -j 1001 -n 1 \
    ./rw_speed -time=60 -iosize=$iosize -filesize=100m -tag=job1 &> rw_speed1.out &
 
 IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 2)) \
-  ../tests/themis_client.sh -r 300 -u 102 -j 1002 -n 1 -s 15  \
+  ../tests/themis_client.sh -r 20000 -u 102 -j 1002 -n 1 -s 15  \
   ./rw_speed -time=30 -iosize=$iosize -filesize=100m -tag=job2 &> rw_speed2.out &
 
 wait
