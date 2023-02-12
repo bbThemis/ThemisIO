cd1f1abc60aedfa4ab4a9daee0caf32b79b2b81a
Yuhong Kan <yhkan@c100-007.frontera.tacc.utexas.edu>
2023-02-12 16:02:48 -0600

    change test.1v1.sh

--- testfair/test.1v1.sh
+++ testfair/test.1v1.sh
@@ -3,11 +3,11 @@
 ppn=56
 iosize=1m
 
-IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 1)) \
+IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 2)) \
    ../tests/themis_client.sh -u 101 -j 1001 -n 1 \
    ./rw_speed -time=60 -iosize=$iosize -filesize=100m -tag=job1 &> rw_speed1.out &
 
-IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 2)) \
+IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 3)) \
    ../tests/themis_client.sh -u 102 -j 1002 -n 1 -s 15 \
    ./rw_speed -time=30 -iosize=$iosize -filesize=100m -tag=job2 &> rw_speed2.out &
 
