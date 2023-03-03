440a0838b57b9a6ea2b13e98f7b037f5954d0ec8
Yuhong Kan <yhkan@c100-001.frontera.tacc.utexas.edu>
2023-03-02 18:23:06 -0600

    test

--- testfair/test.1v1.sh
+++ testfair/test.1v1.sh
@@ -7,13 +7,13 @@ IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 1)) \
    ../tests/themis_client.sh -u 101 -j 1001 -n 1 \
    ./rw_speed -time=15 -iosize=$iosize -filesize=100m -tag=job1 &> rw_speed1.out &
 
-IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 2)) \
-   ../tests/themis_client.sh -u 102 -j 1002 -n 1 -s 0 \
-   ./rw_speed -time=15 -iosize=$iosize -filesize=100m -tag=job2 &> rw_speed2.out &
+# IBRUN_TASKS_PER_NODE=$ppn ibrun -n $(($ppn * 1)) -o $(($ppn * 2)) \
+#    ../tests/themis_client.sh -u 102 -j 1002 -n 1 -s 0 \
+#    ./rw_speed -time=15 -iosize=$iosize -filesize=100m -tag=job2 &> rw_speed2.out &
 
 wait
 cat rw_speed1.out
-cat rw_speed2.out
+# cat rw_speed2.out
 
 # server in job-fair mode
 
